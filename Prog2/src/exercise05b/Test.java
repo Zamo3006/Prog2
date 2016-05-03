d3afef19a950d4f4b8b55da53d9f78a2295609a4
Budig <2>
2016-05-03 15:56:46 +0200

    5b

---
+++ Prog2/src/exercise05b/Test.java
@@ -0,0 +1,30 @@
+package exercise05b;
+
+public class Test {
+	
+	private int i;
+	
+	public Test(int i){
+		this.i = i;
+	}
+	public int getI() {
+		return i;
+	}
+public void setI(int i) {
+	this.i = i;
+}
+public static void main(String[] args)  {
+	Test a = new Test(1);
+	Test b = a;
+	System.out.println(a.getI());
+	System.out.println(b.getI());
+	
+	a.setI(3);
+	System.out.println(a.getI());
+	System.out.println(b.getI());
+	b.setI(5);
+	System.out.println(a.getI());
+	System.out.println(b.getI());
+}
+}
+
